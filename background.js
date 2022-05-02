1292faf375ac18aa8a62e3e082a1bddf60bbe236
Chunws <dntkd51@naver.com>
2022-05-02 23:53:09 +0900

    edit css

--- background.js
+++ background.js
@@ -5,6 +5,8 @@ const images = [
 const chose_img = images[Math.floor(Math.random() * images.length)];
 const background_img = document.createElement("img");
 background_img.src = `${chose_img}`;
+background_img.classList.add("backimg");
 
 document.body.appendChild(background_img);
-// console.log(background_img);
+console.log(background_img);
+// document.body.style.backgroundImage = `url(${chose_img})`;
